4213609ac308c61ed91bb82816a9ddc915fb2256
mayajolie <mayangom95@gmail.com>
2019-07-24 08:31:45 +0000

    premier commit

---
+++ src/Entity/Partenaires.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\PartenairesRepository")
+ */
+class Partenaires
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $raisonSocial;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ninea;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $codeP;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRaisonSocial(): ?string
+    {
+        return $this->raisonSocial;
+    }
+
+    public function setRaisonSocial(string $raisonSocial): self
+    {
+        $this->raisonSocial = $raisonSocial;
+
+        return $this;
+    }
+
+    public function getNinea(): ?string
+    {
+        return $this->ninea;
+    }
+
+    public function setNinea(string $ninea): self
+    {
+        $this->ninea = $ninea;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?int
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(int $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getCodeP(): ?string
+    {
+        return $this->codeP;
+    }
+
+    public function setCodeP(string $codeP): self
+    {
+        $this->codeP = $codeP;
+
+        return $this;
+    }
+}
