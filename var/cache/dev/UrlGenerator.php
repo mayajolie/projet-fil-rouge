84858107f0c3f88275c2990371aeeea25651e836
mayajolie <mayangom95@gmail.com>
2019-07-27 12:57:21 +0000

    2 base de donnes

--- var/cache/dev/UrlGenerator.php
+++ var/cache/dev/UrlGenerator.php
@@ -4,16 +4,10 @@
 
 return [
     '_twig_error_test' => [['code', '_format'], ['_controller' => 'twig.controller.preview_error::previewErrorPageAction', '_format' => 'html'], ['code' => '\\d+'], [['variable', '.', '[^/]++', '_format', true], ['variable', '/', '\\d+', 'code', true], ['text', '/_error']], [], []],
-<<<<<<< HEAD
-    'find_partenaires' => [[], ['_controller' => 'App\\Controller\\WariController::index'], [], [['text', '/api/partenaires']], [], []],
-    'add_partenaire' => [[], ['_controller' => 'App\\Controller\\WariController::addPartenaire'], [], [['text', '/api/partenaire']], [], []],
-=======
     'register' => [[], ['_controller' => 'App\\Controller\\SecurityController::register'], [], [['text', '/api/register']], [], []],
     'login' => [[], ['_controller' => 'App\\Controller\\SecurityController::login'], [], [['text', '/api/loginchek']], [], []],
-    'partenaires' => [[], ['_controller' => 'App\\Controller\\WariController::patenaires'], [], [['text', '/api/partenaires']], [], []],
-    'ajout' => [[], ['_controller' => 'App\\Controller\\WariController::AjoutP'], [], [['text', '/api/ajout']], [], []],
-    'aj' => [[], ['_controller' => 'App\\Controller\\WariController::update'], [], [['text', '/api/bloquer']], [], []],
->>>>>>> 72578ce8c226d4dce3dab2524b26d312320622bc
+    'find_partenaires' => [[], ['_controller' => 'App\\Controller\\WariController::index'], [], [['text', '/api/partenaires']], [], []],
+    'add_partenaire' => [[], ['_controller' => 'App\\Controller\\WariController::addPartenaire'], [], [['text', '/api/partenaire']], [], []],
     'api_entrypoint' => [['index', '_format'], ['_controller' => 'api_platform.action.entrypoint', '_format' => '', '_api_respond' => 'true', 'index' => 'index'], ['index' => 'index'], [['variable', '.', '[^/]++', '_format', true], ['variable', '/', 'index', 'index', true], ['text', '/api']], [], []],
     'api_doc' => [['_format'], ['_controller' => 'api_platform.action.documentation', '_format' => '', '_api_respond' => 'true'], [], [['variable', '.', '[^/]++', '_format', true], ['text', '/api/docs']], [], []],
     'api_jsonld_context' => [['shortName', '_format'], ['_controller' => 'api_platform.jsonld.action.context', '_format' => 'jsonld', '_api_respond' => 'true'], ['shortName' => '.+'], [['variable', '.', '[^/]++', '_format', true], ['variable', '/', '.+', 'shortName', true], ['text', '/api/contexts']], [], []],
