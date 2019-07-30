f201ad2db582ca97231a114be0573dcab8420e7a
mayajolie <mayangom95@gmail.com>
2019-07-30 11:10:40 +0000

    fin test fonctionnel avec tous les methodes

--- tests/WaricontrollerTest.php
+++ tests/WaricontrollerTest.php
@@ -130,5 +130,60 @@ class WaricontrollerTest extends WebTestCase
             var_dump($rep);
         $this->assertSame(400, $client->getResponse()->getStatusCode());
     }
- 
+    public function testAddComptBok()
+    {
+        $client = static::createClient([],
+            ['PHP_AUTH_USER' =>"mayajolie",
+            'PHP_AUTH_PW'=>"mayajolie"]
+        
+        );
+        $crawler = $client->request('POST', '/api/comptB',[],[],
+        ['CONTENT_TYPE' => 'application/json'],
+        '{"numeroCompte":1247856552,"solde": "20050000","partenaire": "1"}');
+        $rep = $client->getResponse();
+         var_dump($rep);
+        $this->assertSame(201, $client->getResponse()->getStatusCode());
+    }
+    public function testAddComptB()
+    {
+        $client = static::createClient([],
+            ['PHP_AUTH_USER' =>"mayajolie",
+            'PHP_AUTH_PW'=>"mayajolie"]
+        
+        );
+        $crawler = $client->request('POST', '/api/comptB',[],[],
+        ['CONTENT_TYPE' => 'application/json'],
+        '{"numeroCompte":1247856552,"solde":,"partenaire": "1"}');
+        $rep = $client->getResponse();
+         var_dump($rep);
+        $this->assertSame(400, $client->getResponse()->getStatusCode());
+    }
+    public function testAddDepotOK()
+    {
+        $client = static::createClient([],
+            ['PHP_AUTH_USER' =>"mayajolie",
+            'PHP_AUTH_PW'=>"mayajolie"]
+        
+        );
+        $crawler = $client->request('POST', '/api/depot',[],[],
+        ['CONTENT_TYPE' => 'application/json'],
+        '{"montant":50000,"comptb": "1","dateDepot": "2019-07-29 15:33:07"}');
+        $rep = $client->getResponse();
+         var_dump($rep);
+        $this->assertSame(201, $client->getResponse()->getStatusCode());
+    }
+    public function testAddDepot()
+    {
+        $client = static::createClient([],
+            ['PHP_AUTH_USER' =>"mayajolie",
+            'PHP_AUTH_PW'=>"mayajolie"]
+        
+        );
+        $crawler = $client->request('POST', '/api/depot',[],[],
+        ['CONTENT_TYPE' => 'application/json'],
+        '{"montant":50000,"comptb": "1","dateDepot": "2019-07-29"}');
+        $rep = $client->getResponse();
+         var_dump($rep);
+        $this->assertSame(201, $client->getResponse()->getStatusCode());
+    }
 }
